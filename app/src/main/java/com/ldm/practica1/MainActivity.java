68f9df19e7be401d3478dcb77491b483bbf88e98
AlbertoSierraLopez <alberto.sierra360@outlook.com>
2020-10-20 22:56:34 +0200

    Intents para navegar entre activities

--- app/src/main/java/com/ldm/practica1/MainActivity.java
+++ app/src/main/java/com/ldm/practica1/MainActivity.java
@@ -2,7 +2,9 @@ package com.ldm.practica1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -32,5 +34,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void reiniciar(View view) {
+        Intent reiniciar = new Intent(this, MainActivity.class);
+        startActivity(reiniciar);
+    }
+
+    /* METODO PARA DEBUG */
+    public void acabar(View view) {
+        Intent acabar = new Intent(this, ResultadosActivity.class);
+        startActivity(acabar);
     }
 }
--- app/src/main/java/com/ldm/practica1/ResultadosActivity.java
+++ app/src/main/java/com/ldm/practica1/ResultadosActivity.java
@@ -2,7 +2,11 @@ package com.ldm.practica1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
@@ -19,5 +23,24 @@ public class ResultadosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultados);
+    }
+
+    public void reiniciar(View v) {
+        Intent reiniciar = new Intent(this, MainActivity.class);
+        startActivity(reiniciar);
+    }
+
+    public void terminar(View v) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            this.finishAndRemoveTask();
+        } else {
+            this.finish();
+        }
+        System.exit(0);
+    }
+
+    public void compartir(View v) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.twitter.com"));
+        startActivity(browserIntent);
     }
 }
