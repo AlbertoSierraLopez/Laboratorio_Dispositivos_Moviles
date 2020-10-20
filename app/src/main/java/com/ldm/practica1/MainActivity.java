fba24c749f68aec4dc145cf2e13f3c20a3a3439c
AlbertoSierraLopez <alberto.sierra360@outlook.com>
2020-10-20 23:23:05 +0200

    Inicializar variables

--- app/src/main/java/com/ldm/practica1/MainActivity.java
+++ app/src/main/java/com/ldm/practica1/MainActivity.java
@@ -6,23 +6,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private TextView numeroPregunta;
     private TextView pregunta;
     private Button reiniciar;
     private Button siguiente;
+    private ListView respuestas;
 
-    private int contadorPreguntas = 1;
+    private int contadorPreguntas = 0;
     private int puntuacion = 0;
+
+    private LibreriaQuiz libreria = new LibreriaQuiz();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        numeroPregunta = (TextView) findViewById(R.id.txtNumeroPregunta);
+        pregunta = (TextView) findViewById(R.id.txtPregunta);
+        reiniciar = (Button) findViewById(R.id.btnSiguiente);
+        siguiente = (Button) findViewById(R.id.btnReiniciar);
+        respuestas = (ListView) findViewById(R.id.listViewRespuestas);
+
     }
 
     public void reiniciar(View view) {
