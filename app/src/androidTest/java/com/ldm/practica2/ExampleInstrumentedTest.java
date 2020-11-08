d7f04338961714ebccf3ca9f4c0528d461d79a39
AlbertoSierraLopez <alberto.sierra360@outlook.com>
2020-11-08 21:24:34 +0100

    Refactor practica1 -> practica2

--- app/src/androidTest/java/com/ldm/practica2/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/ldm/practica2/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.ldm.practica1", appContext.getPackageName());
+        assertEquals("com.ldm.practica2", appContext.getPackageName());
     }
 }
