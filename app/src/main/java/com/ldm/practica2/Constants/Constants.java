2647d7aecbf8bb6dbf8ed99e7a967b57cf616ad2
AlbertoSierraLopez <alberto.sierra360@outlook.com>
2020-12-19 21:07:22 +0100

    JuegoPiratas

--- app/src/main/java/com/ldm/practica2/Constants/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.ldm.practica2.Constants;
-
-public class Constants {
-    public static final int N_PREGUNTAS = 5;
-    public static final String DATABASE_NAME = "DBPreguntas.db";
-    public static final String DATABASE_TABLE_NAME = "preguntas";
-    public static final String Cebolla = "Cebolla";
-}
