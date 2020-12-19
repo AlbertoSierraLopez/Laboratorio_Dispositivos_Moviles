7b94b3370da6f85e0edf1a439e596cfc3dce18c9
AlbertoSierraLopez <alberto.sierra360@outlook.com>
2020-12-19 20:55:09 +0100

    Sebolla

--- app/src/main/java/com/ldm/practica2/Constants/Constants.java
+++ app/src/main/java/com/ldm/practica2/Constants/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
     public static final int N_PREGUNTAS = 5;
     public static final String DATABASE_NAME = "DBPreguntas.db";
     public static final String DATABASE_TABLE_NAME = "preguntas";
+    public static final String Cebolla = "Cebolla";
 }
